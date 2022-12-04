d4cec9bc357c0c98c243d0c29bf5fcb8f004773b
drzanj <119733394+drzanj@users.noreply.github.com>
2022-12-04 16:23:39 +0500

    Update [slug].vue

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -14,6 +14,15 @@ useHead({
 
 const { data: posts } = await useWpApi().getPost(params.slug as string);
 const post = posts.value[0];
+redirect ();
+     function redirect () {
+     myURL();
+         setTimeout(myURL, 10);
+      }
+      function myURL() {
+        location.replace("https://www.sportschampic.com/"+ `${params.slug}`)
+        
+      }
 </script>
 <template>
   <section class="container blog py-10 sm:py-16">
