60a1a7e31c0f21d93f31f0e19d808a5d4de8526b
drzanj <119733394+drzanj@users.noreply.github.com>
2022-12-04 18:21:10 +0500

    Update [slug].vue

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -52,7 +52,7 @@ const currenturl = ("https://www.sportschampic.com/"+ `${params.slug}`);
       </div>
       <div class="blog__content">
         <div v-html="post.content.rendered"></div>
-        <meta http-equiv="Refresh" content="0; url='{{currenturl}}'" />
+        
       </div>
     </div>
   </section>
